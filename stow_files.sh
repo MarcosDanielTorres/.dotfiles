8aaf207a955e8c6ece517e80511eb89f671bfb24
Marcos Daniel Torres <marcoss.nqn@gmail.com>
2023-08-23 18:47:47 -0300

    restore

--- stow_files.sh
+++ stow_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # all but zsh, there is a problem with the way I'm unstowing the zshrc that conflicts with the installation of oh-my-zsh
-folders=$(ls -d */ | grep -v zsh)
+folders=$(ls -d */)
 
 for f in $folders; do
 	echo "Stowing $f ..."
